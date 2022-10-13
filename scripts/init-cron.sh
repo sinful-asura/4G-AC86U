cd3a577abc4fb2bb3df4d3af94468f432f3463c9
Kristijan Jovic <it.kristijan.jovic@gmail.com>
2022-10-13 21:02:54 +0200

    Update init-cron.sh

--- scripts/init-cron.sh
+++ scripts/init-cron.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cru a WANAutoCheck "* * * * * /jffs/scripts/wan-check.sh"
 cru a CRONStatus "*/5 * * * * /jffs/scripts/cron-status.sh"
+logger "[Init Cron] WANAutoCheck & CRONStatus scripts started"
